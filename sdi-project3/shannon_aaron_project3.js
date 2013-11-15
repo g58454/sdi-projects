70723c8a89c03b80160768371c7e39678f4f5215
g58454 <ashannon@fullsail.edu>
2013-11-14 23:56:40 -0600

    Main Meat
    I have completed the main 'meat' of my program, and will complete the
    array and JSON functions soon

--- sdi-project3/shannon_aaron_project3.js
+++ sdi-project3/shannon_aaron_project3.js
@@ -7,7 +7,7 @@
 //Declaring my global variables
 
 var levelName = "Grown Over";
-var enemyCount = 0;
+var lifeMeter = 100;
 var levelDifficulty = prompt("What Level do you want?\nEasy, Medium, or Hard?");
 var playerDead = false;
 
@@ -17,27 +17,25 @@ var playerDead = false;
 /*First function allows for a number of enemies to be assosiated with a local variable
 depending on the input from the user
 */
+
 var levelEasy = function(){
     if (levelDifficulty === "Easy") {
         //code
         console.log("You chose " + levelDifficulty);
-        var enemyCount = 17;
-        return enemyCount;
     }else{
         levelMedium(levelDifficulty);
     }
 }
 
+
+
 /*In addition to the same set up as the levelEasy, this is a function that is called upon
 when the string isn't met from the previous function*/
-
 
 var levelMedium = function(){
     if (levelDifficulty === "Medium") {
         //code
         console.log("You chose " + levelDifficulty);
-        var enemyCount = 27;
-        return enemyCount;
     }else{
         levelHard(levelDifficulty);
     }
@@ -49,26 +47,30 @@ var levelHard = function(){
     if (levelDifficulty === "Hard") {
         //code
         console.log("You chose " + levelDifficulty);
-        var enemyCount = 59;
-        return enemyCount;
     }else{
         alert("You must be a scardy cat");
-        return enemyCount = 0;
     }
 }
 
 
 
-
-
-var batlleDamage = function(levelDifficulty){
-
+//This is a for loop that keeps the player battling.
+var battleDamage = function(){
+    alert("Your Health is at " + lifeMeter);
+    var fightStatus = confirm("Would you like to battle?");
+    if (fightStatus === true) {
+        //code
+        while (lifeMeter, lifeMeter >= 0, lifeMeter--){
+            //code
+            console.log("Your health is now " + lifeMeter);
+        }
+    }else{
+        console.log("Go home!")
+    }
 }
-
-
-
 
 
 
 //Calling functions in proper order with the proper arguments
 levelEasy(levelDifficulty);
+battleDamage(lifeMeter);
